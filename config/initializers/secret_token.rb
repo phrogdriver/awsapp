0d15273beee42eeeeef883638db301a88eb801a7
Adam Gengler <ajgeng@gmail.com>
2014-04-17 17:56:47 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Awsapp::Application.config.secret_key_base = 'bc49767703b9faa15ead9f9b02e5e0d8dbf2c7fe4d9f3bdb29f15cc45a61bb054a476ffe949dd14479745cd31adbfa4b91b1f1f6835e357e695df8854d80dcec'
